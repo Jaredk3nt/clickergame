da2ddd76a96bd46e37983bce92a9d4831e9a7a8a
Jared Jones <jkjones.dev@gmail.com>
2018-05-09 18:35:11 -0500

    fixing router errors to allow for history and location to be passed into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom'
 import './styles/App.css';
 
 import LandingPage from './organisms/LandingPage/LandingPage';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props.location);
+    }
 
     render() {
         return (
-            <Router>
-                <div className="App">
-                    <Switch>
-                        <Route exact path="/" component={LandingPage}/>
-                    </Switch>
-                </div>
-            </Router>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={LandingPage}/>
+                </Switch>
+            </div>
         );
     }
 }
 
-export default App;
+const AppWithRoute = withRouter(App);
+export default AppWithRoute;
